e04a3ee141250ddd5e3dee5bf1fe495523ffa66d
Christian Keng <1011042@apsva.us>
2025-04-09 09:20:54 -0400

    hi

---
+++ js_circle.js
@@ -0,0 +1,16 @@
+const canvas = document.getElementById("myCanvas");
+const ctx = canvas.getContext("2d") 
+
+
+function circle(x,y){
+ctx.strokeStyle = "red";
+ctx.lineWidth=10;
+ctx.beginPath();
+ctx.arc(x,y,50,0,6.28);
+ctx.stroke();
+ctx.fill();
+}
+circle(100, 100);
+circle(200, 50);
+circle(175, 15);
+circle(30,30);
---
+++ js_function.js
@@ -0,0 +1,12 @@
+function hi(name){
+	console.log("hi"+ name);
+}
+
+hi("Chris");
+
+function f(x,y){
+	console.log(x+y);
+}
+
+f(3,5);
+f(8,6);
